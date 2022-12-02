da4ecb98ec2ab9ae0630673c3b00f37d3473087e
dexterrkk2 <95152097+dexterrkk2@users.noreply.github.com>
2022-12-01 20:28:45 -0700

    photon

--- Cubethon/Assets/Scripts/GameManager.cs
+++ Cubethon/Assets/Scripts/GameManager.cs
@@ -2,13 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class GameManager : MonoBehaviour
+public class GameManager : MonoBehaviourPunCallbacks
 {
 
     bool gameHasEnded = false;
     public float restartDelay = 1f;
     public GameObject completeLevelUI;
+    [Header("Players")]
+    public string playerPrefabLocation; // path in Resources folder to the Player prefab
+    public Transform[] spawnPoints; // array of all available spawn points
+    public PlayerMvmt[] players; // array of all the players
+    public float respawnTime;
+    private int playersInGame; // number of players in the game
+    // instance
+    public static GameManager instance;
 
     public void EndGame()
     {
@@ -16,10 +26,17 @@ public class GameManager : MonoBehaviour
         {
             gameHasEnded = true;
             Debug.Log("Gameover");
-            Invoke("Restart", restartDelay);
+            //Invoke("Restart", restartDelay);
         }
     }
-
+    void SpawnPlayer()
+    {
+        // instantiate the player across the network
+        GameObject playerObj = PhotonNetwork.Instantiate(playerPrefabLocation, spawnPoints[Random.Range(0, spawnPoints.Length)].position, Quaternion.identity);
+        // get the player script
+        PlayerMvmt player = playerObj.GetComponent<PlayerMvmt>();
+        playerObj.GetComponent<PlayerMvmt>().photonView.RPC("Initialize", RpcTarget.All, PhotonNetwork.LocalPlayer);
+    }
     void Restart()
     {
 
--- Cubethon/Assets/Scripts/PlayerMvmt.cs
+++ Cubethon/Assets/Scripts/PlayerMvmt.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Photon.Pun;
+using Photon.Realtime;
 public class PlayerMvmt : MonoBehaviour
 {
     public Rigidbody rb;
     public float forwardForce = 2000f;
     public float sidewaysForce = 500f;
     public float upwardForce = 50f;
+    public float maxY;
+    public float maxX;
+    [Header("Photon")]
+    public int id;
+    public Player photonPlayer;
+    public static PlayerMvmt instance;
+    private void Awake()
+    {
+        instance = this;
+    }
     // Update is called once per frame
     void FixedUpdate ()
     {
@@ -23,7 +34,6 @@ public class PlayerMvmt : MonoBehaviour
             rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
 
-
         if (Input.GetKey("w"))
         {
             rb.AddForce(0, upwardForce * Time.deltaTime, 0, ForceMode.VelocityChange);
@@ -34,22 +44,22 @@ public class PlayerMvmt : MonoBehaviour
             rb.AddForce(0, -upwardForce * Time.deltaTime, 0, ForceMode.VelocityChange);
         }
 
-        if(rb.position.y < -25f)
+        if(rb.position.y < -maxY)
         {
             FindObjectOfType<GameManager>().EndGame();
         }
 
-        if (rb.position.y > 25f)
+        if (rb.position.y > maxY)
         {
             FindObjectOfType<GameManager>().EndGame();
         }
 
-        if (rb.position.x < -25f)
+        if (rb.position.x < -maxX)
         {
             FindObjectOfType<GameManager>().EndGame();
         }
 
-        if (rb.position.x > 25f)
+        if (rb.position.x > maxX)
         {
             FindObjectOfType<GameManager>().EndGame();
         }
